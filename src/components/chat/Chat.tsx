6606767d29f7d13dfe2a3d3f11978113d9d8c0dd
unknown <asmatol@yandex.ru>
2022-09-22 03:24:38 +0300

    add castom scroll

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,15 +2,23 @@ import React, {FC, useEffect, useRef, useState} from 'react';
 import {Message} from "./message/Message";
 import {IMessage} from "../../types/types";
 import s from "./chat.module.scss"
-import { Form } from './form/Form';
+import {Form} from './form/Form';
 
 export const Chat: FC = () => {
     const [value, setValue] = useState('')
     const [message, setMessage] = useState<IMessage[]>([])
     const inputRef = useRef<HTMLInputElement>(null)
+    const messageListRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        console.log(inputRef.current)
+        // @ts-ignore
+        if (messageListRef.current?.clientHeight > 370) {
+            console.log('f')
+            messageListRef.current?.scrollBy(0, 1000)
+        }
+    },[message])
+
+    useEffect(() => {
         if (message.at(-1)?.author === 'User: ') {
             const timeout = setTimeout(() => {
                 setMessage([...message,
@@ -21,7 +29,6 @@ export const Chat: FC = () => {
             }
         }
     }, [message])
-
 
 
     const addMessage = () => {
@@ -38,7 +45,7 @@ export const Chat: FC = () => {
             <div className={s.send}>
                 <Form value={value} inputRef={inputRef} setValue={setValue} addMessage={addMessage}/>
             </div>
-            <Message message={message}/>
+            <Message messageListRef={messageListRef} message={message}/>
         </div>
     );
 }
--- src/components/chat/message/Message.tsx
+++ src/components/chat/message/Message.tsx
@@ -5,20 +5,20 @@ import s from "./message.module.scss"
 
 interface MessageProps {
     message: IMessage[]
+    messageListRef: any
 }
 
-export const Message = ({message}: MessageProps) => {
+export const Message = ({message, messageListRef}: MessageProps) => {
 
     return (
-        <div className={s.listItems}>
+        <div ref={messageListRef} className={s.listItems}>
             {message.map((el: IMessage) => (
-                <p
+                <div
                     className={s.listItem}
                     key={nanoid()}>
-                    {el.author} {el.text}
-                </p>
-                ))
-            }
+                    <span className={s.author}>{el.author}</span>  <span className={s.text}>{el.text}</span>
+                </div>
+                ))}
         </div>
     );
 }
